1c9cb67268b7ea577af9a5e5e547cf9334169e7d
Stalin Francis <educaysoft@hotmail.com>
2017-08-29 13:13:35 -0500

    Corregi la 5 linea
    Se cambio la palabra Go  por Acceder

--- application/config/language/spanish/login_lang.php
+++ application/config/language/spanish/login_lang.php
@@ -2,7 +2,7 @@
 $lang['login_login']='Login';
 $lang['login_username']='Usuario';
 $lang['login_password']='Contraseña';
-$lang['login_go']='Go';
+$lang['login_go']='Acceder';
 $lang['login_invalid_username_and_password']='Incorrecto Usuario/Contraseña';
 $lang['login_welcome_message']='Bienvenido al Punto de venta de Distribuidora los Angeles Para continuar por favor ingrese su usuario y contraseña. <br>'; 
 ?>
