f7e870838306c438ca6a845d25d312112b044643
davidchavez1992 <31449702+davidchavez1992@users.noreply.github.com>
2017-08-30 13:17:53 -0500

    Add files via upload

--- application/config/language/spanish/customers_lang.php
+++ application/config/language/spanish/customers_lang.php
@@ -1,16 +1,16 @@
 <?php
-$lang['customers_new']='New Customer';
-$lang['customers_customer']='Customer';
-$lang['customers_update']='Update Customer';
+$lang['customers_new']='Nuevo Cliente';
+$lang['customers_customer']='Cliente';
+$lang['customers_update']='Actualizar Cliente';
 $lang['customers_confirm_delete']='Are you sure you want to delete the selected customers?';
 $lang['customers_none_selected']='You have not selected any customers to delete';
-$lang['customers_error_adding_updating'] = 'Error adding/updating customer';
-$lang['customers_successful_adding']='You have successfully added customer';
-$lang['customers_successful_updating']='You have successfully updated customer';
+$lang['customers_error_adding_updating'] = 'Error adding/updating Cliente';
+$lang['customers_successful_adding']='You have successfully added Cliente';
+$lang['customers_successful_updating']='You have successfully updated Cliente';
 $lang['customers_successful_deleted']='You have successfully deleted';
-$lang['customers_one_or_multiple']='customer(s)';
+$lang['customers_one_or_multiple']='Cliente(s)';
 $lang['customers_cannot_be_deleted']='Could not deleted selected customers, one or more of the selected customers has sales.';
-$lang['customers_basic_information']='Customer Information';
-$lang['customers_account_number']='Account #';
+$lang['customers_basic_information']='Cliente Information';
+$lang['customers_account_number']='Cuenta #';
 $lang['customers_taxable']='Taxable';
 ?>
--- application/config/language/spanish/module_lang.php
+++ application/config/language/spanish/module_lang.php
@@ -1,25 +1,25 @@
 <?php
-$lang['module_home']='Home';
+$lang['module_home']='Inicio';
 
-$lang['module_customers']='Clientes';
-$lang['module_customers_desc']='Agregar, Actualizar, Eliminar, y Buscar Clientes';
+$lang['module_customers']='Cliente';
+$lang['module_customers_desc']='Agregar, Actualizar, Eliminar, Buscar clientes';
 
 $lang['module_suppliers']='Proveedores';
-$lang['module_suppliers_desc']='Agregar, Actualizar, Eliminar, y Buscar proveedores';
+$lang['module_suppliers_desc']='Agregar, Actualizar, Eliminar, Buscar Proveedores ';
 
 $lang['module_employees']='Empleados';
-$lang['module_employees_desc']='Agregar, Actualizar, Eliminar, y Buscar empleados';
+$lang['module_employees_desc']='Agregar, Actualizar, Eliminar, Buscar Empleados';
 
 $lang['module_sales']='Ventas';
-$lang['module_sales_desc']='Elabora facturas y las imprime';
+$lang['module_sales_desc']='Iniciar venta';
 
 $lang['module_reports']='Reportes';
-$lang['module_reports_desc']='Generar e imprimir reportes';
+$lang['module_reports_desc']='Visualizar reporte';
 
-$lang['module_items']='Producto';
-$lang['module_items_desc']='Agredar, Actualizar, Eliminar, y Buscar producto';
+$lang['module_items']='Poductos';
+$lang['module_items_desc']='Agregar, Actualizar, Eliminar, Buscar Poductos ';
 
-$lang['module_config']='Configuración general';
-$lang['module_config_desc']='Cambiar configuraciones de forma general';
+$lang['module_config']='Configuracion';
+$lang['module_config_desc']='Cambiar configuracion';
 
 ?>
